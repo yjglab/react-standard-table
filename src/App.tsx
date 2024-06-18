643403103de340803e10666019c980e641868438
jkyukarts <jkyuk@i-screamarts.com>
2024-06-18 18:58:55 +0900

    chore(table): adjusting loaded data location

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TableContainer from "./lib/components/table/TableContainer";
 
+const headItems = [
+  { id: "1", content: "abc" },
+  { id: "2", content: "def" },
+  { id: "3", content: "ghi" },
+  { id: "4", content: "jkh" },
+];
+
+const row1Items = [
+  {
+    id: "1",
+    content: "1행1열",
+  },
+  {
+    id: "2",
+    content: <div className="text-cyan-400">1행2열컴포넌트</div>,
+    className: "col-span-3",
+  },
+  {
+    id: "3",
+    content: "1행3열",
+  },
+  {
+    id: "4",
+    content: <div className="text-red-400">1행4열컴포넌트</div>,
+  },
+];
+
 function App() {
+  const [data, setData] = useState([]);
+
+  // only test
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/comments").then((response) =>
+      response.json().then((res) => setData(res))
+    );
+  }, []);
+
+  data && console.log(data);
   return (
     <div className="min-h-[400px]">
-      <TableContainer />
+      <TableContainer headItems={headItems} data={} />
     </div>
   );
 }
--- src/lib/components/table/TableContainer.tsx
+++ src/lib/components/table/TableContainer.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import TableOption from "./TableOption";
 import { cn } from "../../utils/cn";
 import TableHead from "./TableHead";
@@ -7,37 +7,12 @@ import TableRow from "./TableRow";
 import Paginator from "../partials/Paginator";
 
 interface Props {
+  headItems: any;
+  data: any;
   className?: string;
 }
 
-const headItems = [
-  { id: "1", content: "abc" },
-  { id: "2", content: "def" },
-  { id: "3", content: "ghi" },
-  { id: "4", content: "jkh" },
-];
-
-const row1Items = [
-  {
-    id: "1",
-    content: "1행1열",
-  },
-  {
-    id: "2",
-    content: <div className="text-cyan-400">1행2열컴포넌트</div>,
-    className: "col-span-3",
-  },
-  {
-    id: "3",
-    content: "1행3열",
-  },
-  {
-    id: "4",
-    content: <div className="text-red-400">1행4열컴포넌트</div>,
-  },
-];
-
-const TableContainer: FC<Props> = ({ className = "" }) => {
+const TableContainer: FC<Props> = ({ headItems, data, className = "" }) => {
   const [page, setPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const total = 30; // data.length
